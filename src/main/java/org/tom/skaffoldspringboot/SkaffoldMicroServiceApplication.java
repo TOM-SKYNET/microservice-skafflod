67295a7d74f69ea0f13c51abddb6edb58b188af3
TOM-SKYNET <netmail.tom@gmail.com>
2019-11-16 21:01:35 +0000

    2

---
+++ src/main/java/org/tom/skaffoldspringboot/SkaffoldMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package org.tom.skaffoldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaffoldMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaffoldMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tom/skaffoldspringboot/SkaffoldMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tom.skaffoldspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaffoldMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
